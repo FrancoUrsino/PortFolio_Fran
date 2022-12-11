69d12d40b65a089890dccf1e9e15e3e3b8938bed
Franco Ursino <franncoursinno@gmail.com>
2022-12-11 15:11:15 -0300

    fix height

--- js/nav.js
+++ js/nav.js
@@ -6,14 +6,3 @@ navbar.addEventListener('click', function(){
   this.classList.toggle('click');
   navlist.classList.toggle('open');
 })
-
-
-let navigation = document.querySelector('.navigation');
-let close = document.querySelector('.close');
-
-navigation.onclick = function(){
-  navigation.classList.add('active');
-}
-close.onclick = function(){
-  navigation.classList.remove('active');
-}
